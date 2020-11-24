204c885f06398c43efee10ad4099051674bceab5
iquzart <iquzart@hotmail.com>
2020-11-24 17:55:38 +0400

    multiple fixes and new flow

--- notification/slack.py
+++ notification/slack.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import json
 import jinja2
@@ -6,15 +7,60 @@ import logging
 logging = logging.getLogger(__name__)
 
 class NotifySlack:
-    def __init__(self, slack_token, slack_channel, slack_icon_url, db):
+    """
+    A class to interact with slack.
 
+    ...
+
+    Attributes
+    ----------
+    slack_token : str
+        slack bot token to connect
+    slack_channel : str
+        slack channel to send notification on
+    slack_icon_url : str
+        small logo for slack block message.  
+    notification_data: dict
+        the dict containes all vars to create slack block message.
+
+    Methods
+    -------
+    send_message(self):
+        send the message to slack on the channel specified.
+    """
+    def __init__(self, slack_token, slack_channel, slack_icon_url, notification_description, **notfication_data):
+        """
+        Constructs all the necessary attributes for the notifyslack object.
+
+        Parameters
+        ----------
+        slack_token : str
+            slack bot token to connect
+        slack_channel : str
+            slack channel to send notification on
+        slack_icon_url : str
+            small logo for slack block message.  
+        notification_description: str
+            desctiption on slack message
+        notification_data: dict
+            the dict containes all vars to create slack block message.
+        """
         self.slack_token = slack_token
         self.slack_channel = slack_channel
         self.slack_icon_url = slack_icon_url
-        self.db = db
+        self.notification_description = notification_description
+        self.task = notfication_data['task']
+        self.database_name = notfication_data['db']
+        self.status = notfication_data['status']
         
-    def failure_message(self):
+    def send_message(self):
+        """
+        load the template and construct the slack block. send it to slack using chat.potmessage api.
 
+        Returns
+        -------
+        slack api call result
+        """
         loader = jinja2.FileSystemLoader('notification/templates')
         env = jinja2.Environment(loader=loader)
         env.filters['json'] = json.dumps
@@ -22,11 +68,11 @@ class NotifySlack:
         blocks = ( 
             template.render({
                 'LOGO_URL' : self.slack_icon_url,
-                'BACKUP_STATUS' : 'Backup Failed',
-                'COMPANY_NAME' : 'Some Test Company',
-                'HOSTNAME' : 'LXORDEVDBV01',
-                'FAILED_ON' : 'Backup',
-                'DB_NAME' : self.db,
+                'BACKUP_STATUS' : self.status,
+                'BU' : self.notification_description,
+                'HOSTNAME' : os.uname()[1],
+                'TASK' : self.task,
+                'DB_NAME' : self.database_name,
             })
         )           
 
